81bdedcdaab4a9711000b7f4b6585fc20ee8fd06
Ichinga Samuel <ichingasamuel@gmail.com>
2024-09-30 17:22:49 +0100

    testdata

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,14 @@
-# import pytest
-# from aiomql import MetaTester, TestData, MetaTrader, Config
-# import MetaTrader5 as mt5
-#
-#
-# @pytest.fixture(scope='session', autouse=True)
-# async def initialize():
-#     config = Config()
-#     acc = config.account_info()
-#     mt5.initialize(**acc)
-#     mt5.login(**acc)
+import pytest
+from aiomql import MetaTester, TestData, MetaTrader, Config
+import MetaTrader5
+
+
+@pytest.fixture(scope='session', autouse=True)
+def config():
+    config = Config(filename='test.json')
+    return config
+
+
+@pytest.fixture(scope='session', autouse=True)
+def metatrader5(config):
+    return MetaTrader5
